eb44323ed5cd73afb0411c7255b91ee00800e3fc
BQ Lab <beaqon@naver.com>
2019-03-07 11:37:54 +0900

    first class

---
+++ 인사하기1/인사하기1/frmInSa.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace 인사하기1
+{
+    partial class frmInSa
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.TextBox();
+            this.btnInsa = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.lblMinute = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.tmrClock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.Location = new System.Drawing.Point(14, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(103, 21);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "홍길동";
+            // 
+            // btnInsa
+            // 
+            this.btnInsa.Location = new System.Drawing.Point(139, 10);
+            this.btnInsa.Name = "btnInsa";
+            this.btnInsa.Size = new System.Drawing.Size(75, 23);
+            this.btnInsa.TabIndex = 1;
+            this.btnInsa.Text = "인사하기";
+            this.btnInsa.UseVisualStyleBackColor = true;
+            this.btnInsa.Click += new System.EventHandler(this.btnInsa_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(139, 39);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "종료";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMessage.Location = new System.Drawing.Point(14, 80);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(247, 179);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "안녕못해요";
+            this.lblMessage.Click += new System.EventHandler(this.lblMessage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "이름";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblHour.Location = new System.Drawing.Point(240, 26);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(42, 21);
+            this.lblHour.TabIndex = 5;
+            this.lblHour.Text = "00:";
+            // 
+            // lblMinute
+            // 
+            this.lblMinute.AutoSize = true;
+            this.lblMinute.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblMinute.Location = new System.Drawing.Point(274, 25);
+            this.lblMinute.Name = "lblMinute";
+            this.lblMinute.Size = new System.Drawing.Size(42, 21);
+            this.lblMinute.TabIndex = 6;
+            this.lblMinute.Text = "00:";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblSecond.Location = new System.Drawing.Point(308, 25);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(34, 21);
+            this.lblSecond.TabIndex = 7;
+            this.lblSecond.Text = "00";
+            this.lblSecond.Click += new System.EventHandler(this.lblSecond_Click);
+            // 
+            // tmrClock
+            // 
+            this.tmrClock.Enabled = true;
+            this.tmrClock.Interval = 1000;
+            this.tmrClock.Tick += new System.EventHandler(this.tmrClock_Tick);
+            // 
+            // frmInSa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 322);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblMinute);
+            this.Controls.Add(this.lblHour);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnInsa);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmInSa";
+            this.Text = "안녕하세요";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lblName;
+        private System.Windows.Forms.Button btnInsa;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Label lblMinute;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Timer tmrClock;
+    }
+}
+
---
+++ 인사하기1/인사하기1/frmInSa.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 인사하기1
+{
+    public partial class frmInSa : Form
+    {
+        public frmInSa()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnInsa_Click(object sender, EventArgs e)
+        {
+            int intHour = DateTime.Now.Hour;
+            int intMinute = DateTime.Now.Minute;
+            int intSecond = DateTime.Now.Second;
+            string strHourMessage = lblName.Text + "님!!"+ Environment.NewLine;
+            strHourMessage += "안녕하세요~~" + Environment.NewLine;
+            if (intHour<6)
+            {
+                strHourMessage += "일어나세요~~~"+Environment.NewLine;
+                strHourMessage += "지금은"+ intHour+"시"+intMinute+"분"+intSecond+"초";
+            } else if(intHour <8)
+            {
+                strHourMessage += "학교가세요~~~" + Environment.NewLine;
+                strHourMessage += "지금은" + intHour + "시" + intMinute + "분" + intSecond + "초";
+            }
+            else if (intHour < 12)
+            {
+                strHourMessage += "밥먹으세요~~~" + Environment.NewLine;
+                strHourMessage += "지금은" + intHour + "시" + intMinute + "분" + intSecond + "초";
+            }
+            else if (intHour < 18)
+            {
+                strHourMessage += "집에가세요~~~" + Environment.NewLine;
+                strHourMessage += "지금은" + intHour + "시" + intMinute + "분" + intSecond + "초";
+            }
+            else if (intHour < 20)
+            {
+                strHourMessage += "꿀잠자세요~~~" + Environment.NewLine;
+                strHourMessage += "지금은" + intHour + "시" + intMinute + "분" + intSecond + "초";
+            }
+            else
+            {
+                strHourMessage += "꿀잠자세요~~~" + Environment.NewLine;
+                strHourMessage += "지금은" + intHour + "시" + intMinute + "분" + intSecond + "초";
+            }
+            lblMessage.Text = strHourMessage;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrClock_Tick(object sender, EventArgs e)
+        {
+            lblHour.Text = DateTime.Now.Hour+":";
+            lblMinute.Text = DateTime.Now.Minute + ":";
+            lblSecond.Text = DateTime.Now.Second + ":";
+          
+        }
+
+        private void lblMessage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSecond_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
